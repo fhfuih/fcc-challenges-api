68a4a2401d3647d430de8d0de776ad0bfee3b48c
fhfuih <sam.zyhuang@outlook.com>
2019-08-19 23:45:22 +0800

    Prevent redundant creaton of short url

--- src/routes/shortUrl/shortUrl.controller.js
+++ src/routes/shortUrl/shortUrl.controller.js
@@ -3,6 +3,16 @@ const ShortUrl = require('./shortUrl.model')
 
 async function create (req, res) {
   const { url } = req.body
+
+  const exsisted = await ShortUrl.findOne({ url })
+  if (exsisted) {
+    res.json({
+      url,
+      short: exsisted._id
+    })
+    return
+  }
+
   const doc = new ShortUrl({ url })
   await doc.save()
   res.json({
