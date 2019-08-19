da2969bf727bc55620590218365a24140c3dc2e1
fhfuih <sam.zyhuang@outlook.com>
2019-08-19 23:44:44 +0800

    Add CORS

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,12 @@
 const router = require('express-promise-router')()
+const cors = require('cors')
 const bodyParser = require('body-parser')
 const mongoSanitize = require('express-mongo-sanitize')
 const timestamp = require('./timestamp/timestamp.routes')
 const whoAmI = require('./whoAmI/whoAmI.routes')
 const shortUrl = require('./shortUrl/shortUrl.routes')
 
+router.use(cors())
 router.use(bodyParser.json({ strict: false }))
 router.use(mongoSanitize())
 router.use('/api/timestamp', timestamp)
